b9d410654bf32f68682ffbf2bf36ca8832445fa0
MoonlightOwl <murky.owl@gmail.com>
2017-08-26 11:26:56 +0200

    fix problems with sqlite and jar building

--- src/main/kotlin/totoro/yui/Yui.kt
+++ src/main/kotlin/totoro/yui/Yui.kt
@@ -17,8 +17,8 @@ import javax.net.ssl.*
 
 object Yui {
     @Suppress("MemberVisibilityCanPrivate")
-            // do not forget to change version in build.gradle
-    val Version = "0.3.9"
+    // do not forget to change version in build.gradle
+    val Version = "0.3.10"
     val Random = Random(System.currentTimeMillis())
 
     fun run() {
