4d7276ecacc5e20a5721b996125b09b4eed3c328
MoonlightOwl <murky.owl@gmail.com>
2017-09-07 23:51:17 +0200

    upgrade `t9` command

--- src/main/kotlin/totoro/yui/actions/T9Action.kt
+++ src/main/kotlin/totoro/yui/actions/T9Action.kt
@@ -3,10 +3,11 @@ package totoro.yui.actions
 import totoro.yui.client.IRCClient
 import totoro.yui.util.YandexSpeller
 
-class T9Action : SensitivityAction("*", "sp", "sc", "spell", "spellcheck", "gramar", "correct", "t9", "9") {
+class T9Action : SensitivityAction("*", "spellcheck", "gramar", "correct", "t9") {
     override fun handle(client: IRCClient, command: Command): Boolean {
-        val phrase = client.history.last(command.chan)?.message
-        val text = phrase?.let { YandexSpeller.correct(it) } ?: "i do not remember, what i need to correct?"
+        val nickname = if (command.args.isNotEmpty()) command.args.first() else command.user
+        val phrase = client.history.lastByUser(command.chan, nickname)
+        val text = phrase?.let { YandexSpeller.correct(it.message) } ?: "i do not remember, what i need to correct?"
         client.send(command.chan, text)
         return true
     }
