943ae786fcda316577bc8b8fd4d0ad8f70cabd93
MoonlightOwl <murky.owl@gmail.com>
2018-01-17 20:41:17 +0200

    version 0.4.2

--- src/main/kotlin/totoro/yui/Yui.kt
+++ src/main/kotlin/totoro/yui/Yui.kt
@@ -16,7 +16,7 @@ import javax.net.ssl.*
 
 object Yui {
     // do not forget to change version in build.gradle
-    private val Version = "0.4.1"
+    private val Version = "0.4.2"
     val Random = Random(System.currentTimeMillis())
 
     fun run() {
