321e1a6163391595bd315cc65ceecb965c583402
MoonlightOwl <murky.owl@gmail.com>
2018-01-17 20:38:16 +0200

    add localized wiki search

--- src/main/kotlin/totoro/yui/actions/WikiAction.kt
+++ src/main/kotlin/totoro/yui/actions/WikiAction.kt
@@ -5,17 +5,32 @@ import totoro.yui.client.IRCClient
 import totoro.yui.util.F
 import totoro.yui.util.api.Wiki
 
-class WikiAction : SensitivityAction("w", "wiki", "wikipedia") {
+class WikiAction : SensitivityAction("w", "wiki", "wikipedia", "!^w:..$", "!^wiki:..$", "!^wikipedia:..$") {
     override fun handle(client: IRCClient, command: Command): Boolean {
-        Wiki.search(command.content,
+        when {
+            // check if the country prefix is a part of the command itself
+            command.name?.contains(':') == true ->
+                search(client, command.chan, command.content, command.name.takeLast(2))
+
+            // check if the country prefix is the first word
+            (command.args.firstOrNull()?.startsWith(':') == true) &&
+            (command.args.first().length == 3) ->
+                search(client, command.chan, command.content.drop(4), command.args.first().drop(1))
+
+            else -> search(client, command.chan, command.content, "en")
+        }
+        return true
+    }
+
+    private fun search(client: IRCClient, channel: String, content: String, country: String) {
+        Wiki.search(content, country,
                 { article ->
-                    client.send(command.chan, F.Yellow + article.title + F.Reset + " / " + article.url)
-                    client.send(command.chan, F.Italic + article.snippet + F.Reset)
+                    client.send(channel, F.Yellow + article.title + F.Reset + " / " + article.url)
+                    client.send(channel, F.Italic + article.snippet + F.Reset)
                 },
                 {
-                    client.send(command.chan, F.Gray + "cannot find anything on the topic" + F.Reset)
+                    client.send(channel, F.Gray + "cannot find anything on the topic" + F.Reset)
                 }
         )
-        return true
     }
 }
--- src/main/kotlin/totoro/yui/util/api/Wiki.kt
+++ src/main/kotlin/totoro/yui/util/api/Wiki.kt
@@ -4,13 +4,10 @@ import com.beust.klaxon.*
 import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.result.Result
 import totoro.yui.util.api.data.WikiArticle
-import java.net.URLEncoder
 
 object Wiki {
-    private val charset = "UTF-8"
-
-    fun search(request: String, success: (WikiArticle) -> Unit, failure: () -> Unit) {
-        ("https://en.wikipedia.org/w/api.php?action=query&generator=search&gsrsearch=${URLEncoder.encode(request, charset)}" +
+    fun search(request: String, country: String, success: (WikiArticle) -> Unit, failure: () -> Unit) {
+        ("https://$country.wikipedia.org/w/api.php?action=query&generator=search&gsrsearch=$request" +
             "&gsrlimit=1&prop=extracts|info&inprop=url&exintro&explaintext&exchars=400&format=json&utf8=true")
                 .httpGet().responseString { _, _, result ->
                     when (result) {
