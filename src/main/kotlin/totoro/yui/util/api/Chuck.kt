2b16fe882c3f007b8c50ba830829cf049851f7cf
MoonlightOwl <murky.owl@gmail.com>
2018-01-17 20:37:48 +0200

    remove unnecessary jsoup call from `chuck` action

--- src/main/kotlin/totoro/yui/util/api/Chuck.kt
+++ src/main/kotlin/totoro/yui/util/api/Chuck.kt
@@ -6,8 +6,6 @@ import com.beust.klaxon.obj
 import com.beust.klaxon.string
 import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.result.Result
-import com.github.kittinunf.result.getAs
-import org.jsoup.Jsoup
 
 object Chuck {
     fun quote(success: (String) -> Unit, failure: () -> Unit) {
@@ -15,8 +13,7 @@ object Chuck {
             when (result) {
                 is Result.Failure -> failure()
                 is Result.Success -> {
-                    val raw = Jsoup.parse(result.getAs<String>()).text()
-                    val json = Parser().parse(StringBuilder(raw)) as JsonObject
+                    val json = Parser().parse(StringBuilder(result.value)) as JsonObject
                     val quote = json.obj("value")?.string("joke")
                     if (quote != null) success(quote)
                     else failure()
