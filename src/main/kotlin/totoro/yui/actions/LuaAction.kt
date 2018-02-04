c3ea7f57a6b2944eb48d04ae89ca07c734e3f2d4
MoonlightOwl <murky.owl@gmail.com>
2018-02-04 18:04:15 +0200

    limit the maximum number of output lines of the `lua` command

--- src/main/kotlin/totoro/yui/actions/LuaAction.kt
+++ src/main/kotlin/totoro/yui/actions/LuaAction.kt
@@ -6,6 +6,7 @@ import totoro.yui.util.F
 import totoro.yui.util.api.Lua
 
 class LuaAction : SensitivityAction("lua", "l", "lua=", "l=") {
+    private val maxOutputLines = 3
     private val errorRegex = "^input:\\d*:\\s.*$".toRegex()
 
     override fun handle(client: IRCClient, command: Command): Boolean {
@@ -16,8 +17,15 @@ class LuaAction : SensitivityAction("lua", "l", "lua=", "l=") {
         val result = Lua.run(code)
         if (result == null)
             client.send(command.chan, "${F.Gray}something went wrong${F.Reset}")
-        else
-            client.send(command.chan, "${if (result.matches(errorRegex)) F.Red else F.Yellow}$result${F.Reset}")
+        else {
+            val lines = result.split('\n')
+            lines.take(maxOutputLines).forEach { line ->
+                client.send(command.chan, "${if (line.matches(errorRegex)) F.Red else F.Yellow}$line${F.Reset}")
+            }
+            if (lines.size > maxOutputLines)
+                client.send(command.chan, "${F.Yellow}(and ${lines.size - maxOutputLines} more...)${F.Reset}")
+        }
+
         return true
     }
 }
