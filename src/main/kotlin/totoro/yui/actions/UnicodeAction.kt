d480a2e048c4f85914cedcbe0056f36d4cc0f65d
MoonlightOwl <murky.owl@gmail.com>
2018-02-05 15:47:38 +0200

    enhance unicode action with code-to-char converter

--- src/main/kotlin/totoro/yui/actions/UnicodeAction.kt
+++ src/main/kotlin/totoro/yui/actions/UnicodeAction.kt
@@ -10,24 +10,28 @@ class UnicodeAction : SensitivityAction("u", "char", "unicode") {
         if (command.args.isEmpty()) {
             client.send(command.chan, "gimme some clues")
         } else {
-            UnicodeTable.get(
-                    command.content,
-                    { list ->
-                        client.send(
+            if (command.content.startsWith("U+")) {
+                try {
+                    val code = Integer.parseInt(command.content.drop(2), 16)
+                    client.send(command.chan, "${command.content}: ${F.Yellow}${code.toChar()}${F.Reset}")
+                } catch (e: Exception) {
+                    client.send(command.chan, "${F.Gray}${F.Reset}")
+                }
+            } else {
+                UnicodeTable.get(command.content, { list ->
+                    client.send(
                             command.chan,
-                            list
-                                .filter { symbol -> symbol.symbol.isNotEmpty() && symbol.description.isNotEmpty() }
-                                .joinToString(", ")
-                                { symbol ->
-                                    "${symbol.description} " +
-                                    "(" + F.Yellow + "${symbol.code}: ${symbol.symbol}" + F.Reset + (if (symbol.symbol.length > 1) "  " else " ") + ")"
-                                }
-                        )
-                    },
-                    {
-                        client.send(command.chan, F.Gray + "no characters found" + F.Reset)
-                    }
-            )
+                            list.filter { symbol -> symbol.symbol.isNotEmpty() && symbol.description.isNotEmpty() }
+                                    .joinToString(", ") { symbol ->
+                                        "${symbol.description} " +
+                                                "(" + F.Yellow + "${symbol.code}: ${symbol.symbol}" + F.Reset +
+                                                (if (symbol.symbol.length > 1) "  " else " ") + ")"
+                                    }
+                    )
+                }, {
+                    client.send(command.chan, F.Gray + "no characters found" + F.Reset)
+                })
+            }
         }
         return true
     }
