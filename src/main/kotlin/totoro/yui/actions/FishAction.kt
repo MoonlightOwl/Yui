a73849ec4b3ea7a380ff09aad44389c947e85619
MoonlightOwl <murky.owl@gmail.com>
2018-02-05 16:12:36 +0200

    fishes multiplier

--- src/main/kotlin/totoro/yui/actions/FishAction.kt
+++ src/main/kotlin/totoro/yui/actions/FishAction.kt
@@ -4,12 +4,22 @@ import totoro.yui.Yui
 import totoro.yui.client.Command
 import totoro.yui.client.IRCClient
 import totoro.yui.util.Dict
-
-private val fishes = Dict.of("><>", "<><", "<>><", "><>>", "---E", "><*>", "<*><")
+import totoro.yui.util.F
 
 class FishAction : SensitivityAction("fish", "-fish") {
+    private val maxRandomFishes = 5
+    private val maxTotalFishes = 20
+    private val fishes = Dict.of("><>", "<><", "<>><", "><>>", "---E", "><*>", "<*><")
+
     override fun handle(client: IRCClient, command: Command): Boolean {
-        client.send(command.chan, fishes(Yui.Random.nextInt(5)).joinToString(" "))
+        val quantity =
+            if (command.args.isNotEmpty()) command.args.first().toIntOrNull()
+            else Yui.Random.nextInt(maxRandomFishes) + 1
+        when {
+            quantity == null ->  client.send(command.chan, "${F.Gray}what kind of fish is it?${F.Reset}")
+            quantity > maxTotalFishes -> client.send(command.chan, "${F.Gray}it's a whole herd!${F.Reset}")
+            else -> client.send(command.chan, fishes(quantity).joinToString(" "))
+        }
         return true
     }
 }
