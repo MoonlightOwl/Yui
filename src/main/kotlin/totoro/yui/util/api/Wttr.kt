8457f46a2b916379cbc1c65caff120c8f8efdc03
MoonlightOwl <murky.owl@gmail.com>
2018-02-05 15:17:47 +0200

    add errors processing to `weather` action

--- src/main/kotlin/totoro/yui/util/api/Wttr.kt
+++ src/main/kotlin/totoro/yui/util/api/Wttr.kt
@@ -13,13 +13,16 @@ object Wttr {
                 is Result.Failure -> failure()
                 is Result.Success -> {
                     val lines = result.value.split('\n').take(7).map { it.replace(escapeCodesRegex, "") }
-                    val loc = lines[0].drop(16).trim()
-                    val condition = lines[2].drop(15).trim()
-                    val temperature = lines[3].drop(15).trim()
-                    val wind = lines[4].drop(15).trim()
-                    val visibility = lines[5].drop(15).trim()
-                    val precipitation = lines[6].drop(15).trim()
-                    success(Weather(loc, condition, temperature, wind, visibility, precipitation))
+                    if (lines[0].startsWith("ERROR")) failure()
+                    else {
+                        val loc = lines[0].split(':')[1].trim()
+                        val condition = lines[2].drop(15).trim()
+                        val temperature = lines[3].drop(15).trim()
+                        val wind = lines[4].drop(15).trim()
+                        val visibility = lines[5].drop(15).trim()
+                        val precipitation = lines[6].drop(15).trim()
+                        success(Weather(loc, condition, temperature, wind, visibility, precipitation))
+                    }
                 }
             }
         }
