acf982c971413f8dcdc1474b458c49625a5dc321
MoonlightOwl <murky.owl@gmail.com>
2018-02-09 21:37:25 +0200

    allow custom rips

--- src/main/kotlin/totoro/yui/actions/RipAction.kt
+++ src/main/kotlin/totoro/yui/actions/RipAction.kt
@@ -12,11 +12,9 @@ class RipAction : SensitivityAction("rip", "rippo", "ripped") {
     private val maxTotalRips = 15
 
     override fun handle(client: IRCClient, command: Command): Boolean {
-        val quantity =
-                if (command.args.isNotEmpty()) command.args.first().toIntOrNull()
-                else Yui.Random.nextInt(maxRandomRips) + 1
+        val fromArgs = command.args.firstOrNull()?.toIntOrNull()
+        val quantity = fromArgs ?: Yui.Random.nextInt(maxRandomRips) + 1
         when {
-            quantity == null ->  client.send(command.chan, "${F.Gray}what kind of rip is it?${F.Reset}")
             quantity > maxTotalRips -> client.send(command.chan, "${F.Gray}it's a whole graveyard!${F.Reset}")
             else -> client.send(command.chan, Dict.Rip(quantity).joinToString(" "))
         }
